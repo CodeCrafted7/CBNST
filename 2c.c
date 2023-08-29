68f140264b884e60dfd0d8a6da1dd4e1a202a30f
Shaishav Gupta <141803885+CodeCrafted7@users.noreply.github.com>
2023-08-29 19:10:00 +0530

    Add files via upload

---
+++ 2c.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+#include<math.h>
+#include <stdlib.h>
+void Roundoff(double p,double v)
+{
+    double val = (v * pow(10, p+1));
+    int temp = val+1e-9;
+    int n = temp%10;
+    if(n>5)
+    {
+        temp+=10;
+        temp-=n;
+        temp/=10;
+        double ans = temp/pow(10, p);
+        printf("\nAfter %lf Roundoff of the Number is : ",p);
+        printf("%lf",ans);
+    }
+    else if(n<5)
+    {
+        temp-=n;
+        temp/=10;
+        double ans = temp/pow(10, p);
+        printf("\nAfter %lf Roundoff of the Number is : ",p);
+        printf("%lf",ans);
+
+    }
+    else
+    {
+        int x = temp/10;
+        int y = x%10;
+        if(y%2==1)
+        {
+            x=x+1;
+        }
+        double k = x/pow(10, p);
+        printf("\nAfter %lf Roundoff of the Number is : ",p);
+        printf("%lf",k);
+    }
+}
+void truncate(double p,double v)
+{
+    double m = pow(10,p);
+    double truncate= (int)(v*m)/m;
+    printf("\nTruncation of a Number is : ");
+    printf("%lf",truncate);
+}
+int main()
+{
+    double x,y;
+    printf("Enter the Value of X and Y : ");
+    scanf("%lf %lf",&x,&y);
+    double temp = x-y;
+    if(temp<0)
+    {
+        temp*=-1;
+    }
+    double ae = temp;
+    double re = (ae/x);
+    double pe = (re*100);
+    printf("\nEnter the Number How many decimal point do you want to Roundoff the Value : ");
+    double p =0;
+    scanf("%lf",&p);
+    Roundoff(p,ae);
+    truncate(p,ae);
+   /* Roundoff(p,re);
+    Roundoff(p,pe);*/
+    return 0;
+}
